0d96795ee3298be6dcae1c78bac18702ef85b1ca
Samantha Sterner <samanthalsterner@gmail.com>
2017-12-19 14:07:42 -0500

    Made changes

--- ACSL/ACSL/Program.cs
+++ ACSL/ACSL/Program.cs
@@ -50,10 +50,9 @@ namespace ACSL
 
 
 
-
-
-
-
+            int starttot = 0;
+            int p1end = 0;
+            int p2end = 0;
             for (int i = 1; i < lines.Length; i++)
             {
                 string[] parts = lines[i].Replace(" ", "").Split(new char[] { ',' });
@@ -61,12 +60,13 @@ namespace ACSL
                 List<int> p1 = new List<int>(p1c);
                 List<int> p2 = new List<int>(p2c);
                 total = Convert.ToInt32(parts[0].Trim());
-
+            
                 for (int k = 1; k < parts.Length; k++)
                 {
                     p1.Sort();
                     p2.Sort();
 
+                    starttot = total;
                     int play1 = p1[2];
                     int play2 = p2[2];
                     if (play1 == 9)
@@ -79,41 +79,67 @@ namespace ACSL
                     }
                     else if (play1 == 7)
                     {
-                        if (total >= 92)
+                        if (total + 7 > 99)
                         {
-                            total = total + 1;
+                          total = total + 1;
                         }
                         else
                         {
                             total = total + 7;
                         }
+                      
                     }
                     else
                     {
                         total = total + play1;
                     }
+                    p1end = total;
+                    if(starttot < 33 && p1end > 34)
+                    {
+                        total = total + 5;
+                    }
+                    if(starttot < 55 && p1end > 56)
+                    {
+                        total = total + 5;
+                    }
+                    if(starttot < 77 && p1end > 78)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot > 34 && p1end < 33)
+                    {
+                        total = total + 5;
+                    }
+                    if( starttot > 56 && p1end <55)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot > 78 && p1end < 77)
+                    {
+                        total = total + 5; 
+                    }
+                    starttot = total; 
                     if (total > 99)
                     {
-                        Console.Write(total + ", Player #1");
+                        Console.WriteLine(total + " , player #2 ");
                         break;
-
                     }
 
                     p1[2] = "0123456789TJQKA".IndexOf(parts[k]);
                     p1.Sort();
                     k++;
-
+                    play1 = p1[2];
                     if (play2 == 9)
                     {
                         total = total;
                     }
-                    if (play2 == 10)
+                    else if (play2 == 10)
                     {
                         total = total - 10;
                     }
                     if (play2 == 7)
                     {
-                        if (total >= 92)
+                        if (total + 7 > 99)
                         {
                             total = total + 1;
                         }
@@ -126,15 +152,46 @@ namespace ACSL
                     {
                         total = total + play2;
                     }
+
+                    p2end = total;
+                    if (starttot < 34 && p2end > 33)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot < 56 && p2end > 55)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot < 78 && p2end > 77)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot > 33 && p2end < 34)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot > 55 && p2end < 56)
+                    {
+                        total = total + 5;
+                    }
+                    if (starttot > 77 && p2end < 78)
+                    {
+                        total = total + 5;
+                    }
+                    starttot = total;
+
                     if (total > 99)
                     {
-                        Console.Write(total + ", Player #2");
+                        Console.WriteLine(total + ", Player #1 ");
                         break;
+        
                     }
-                    p1[2] = "0123456789TJQKA".IndexOf(parts[k]);
-                    p1.Sort();
-                    k++;
 
+
+                    p2[2] = "0123456789TJQKA".IndexOf(parts[k]);
+                    p2.Sort();
+                    k++;
+                    play2 = p2[2];
                 }
             }
         }
